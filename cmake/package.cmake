c101f2917c28a11ba22c7bb93eacffec88cc6ae9
Adam Dusty <adam.dusty27@gmail.com>
2024-06-26 19:58:08 -0700

    Update package to only copy necessary dlls to plugin directories

--- cmake/package.cmake
+++ cmake/package.cmake
@@ -18,15 +18,21 @@ add_custom_command(TARGET package POST_BUILD
     COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:chimera_host> ${DIR}
     COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:chimera_sdk> ${DIR}
 
+    # rend
     COMMAND ${CMAKE_COMMAND} -E copy_if_different
         $<TARGET_FILE:chimera::rend> ${DIR}/plugins/chimera/rend/0.1.0
     COMMAND ${CMAKE_COMMAND} -E copy_if_different
         "$<TARGET_FILE_DIR:chimera::rend>/plugin.json" ${DIR}/plugins/chimera/rend/0.1.0
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different
+        $<TARGET_FILE:wgpu> ${DIR}/plugins/chimera/rend/0.1.0
 
+    # core
     COMMAND ${CMAKE_COMMAND} -E copy_if_different
         $<TARGET_FILE:chimera::core> ${DIR}/plugins/chimera/core/0.1.0
     COMMAND ${CMAKE_COMMAND} -E copy_if_different
         "$<TARGET_FILE_DIR:chimera::core>/plugin.json" ${DIR}/plugins/chimera/core/0.1.0
+
+    COMMAND_EXPAND_LISTS
 )
 
 if(WIN32)
